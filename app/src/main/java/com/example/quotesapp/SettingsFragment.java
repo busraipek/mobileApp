25e135f6ec1e1bb08b201218a3622f507ad21733
busraipek <busra.ipek.kmk@gmail.com>
2024-04-20 22:30:02 +0300

    language added

--- app/src/main/java/com/example/quotesapp/SettingsFragment.java
+++ app/src/main/java/com/example/quotesapp/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.quotesapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -7,128 +8,112 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
-import android.widget.Spinner;
 import android.widget.Switch;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.fragment.app.Fragment;
-
 import java.util.Locale;
-public class SettingsFragment<TranslatorOptions> extends Fragment {
 
-    private Spinner languageSpinner;
+public class SettingsFragment extends Fragment {
+
+    private Switch languageSwitch;
     private static final String LANG_NAME = "LanguagePref";
     private static final String LANGUAGE_KEY = "languageKey";
+    private SharedPreferences lsharedPreferences;
 
     private Switch themeSwitch;
     private SharedPreferences sharedPreferences;
     private static final String PREF_NAME = "ThemePref";
     private static final String THEME_KEY = "themeKey";
 
+    @SuppressLint("WrongViewCast")
     @Nullable
     @Override
-
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        languageSpinner = rootView.findViewById(R.id.language_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(),
-                R.array.languages_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        languageSpinner.setAdapter(adapter);
-
         themeSwitch = rootView.findViewById(R.id.theme_switch);
         sharedPreferences = requireActivity().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-
-        // Kaydedilmiş tema tercihini kontrol et ve switch'in durumunu buna göre ayarla
         boolean isDarkMode = sharedPreferences.getBoolean(THEME_KEY, false);
         themeSwitch.setChecked(isDarkMode);
 
+        themeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(THEME_KEY, isChecked);
+            editor.apply();
 
-        // Switch'in durumunu dinleyin ve tema modunu değiştirin
-        themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // Kullanıcının tercihini sakla
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putBoolean(THEME_KEY, isChecked);
-                editor.apply();
-
-                // Tema modunu değiştir
-                if (isChecked) {
-                    // Kullanıcı karanlık tema istediğinde, karanlık moda geçin
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                } else {
-                    // Kullanıcı karanlık tema istemediğinde, aydınlık moda geçin
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                }
-            }
+            AppCompatDelegate.setDefaultNightMode(isChecked ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
         });
 
-
-        // Kaydedilmiş dil tercihini kontrol et ve Spinner'ı buna göre ayarla
-        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences(LANG_NAME, Context.MODE_PRIVATE);
-        String selectedLanguage = sharedPreferences.getString(LANGUAGE_KEY, "");
-        if (!selectedLanguage.isEmpty()) {
-            int position = adapter.getPosition(selectedLanguage);
-            if (position != -1) {
-                languageSpinner.setSelection(position);
-            }
-        }
-
-        // Spinner'daki dil seçimi değiştiğinde işlemleri yap
-        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String language = parent.getItemAtPosition(position).toString();
-                if (language.equalsIgnoreCase("English")) {
-                    changeLanguage("en");
-                } else if (language.equalsIgnoreCase("Türkçe")) {
-                    changeLanguage("tr");
-                }
-                // Diğer dil seçenekleri buraya eklenmeli
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Hiçbir şey yapılmaz
-            }
-        });
+        initThemeSwitch(rootView);
+        initLanguageSwitch(rootView);
 
         return rootView;
     }
-    private void changeLanguage(String languageCode) {
-        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences(LANG_NAME, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+    private void initThemeSwitch(View rootView) {
+        themeSwitch = rootView.findViewById(R.id.theme_switch);
+        sharedPreferences = requireActivity().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        boolean isDarkMode = sharedPreferences.getBoolean(THEME_KEY, false);
+        themeSwitch.setChecked(isDarkMode);
+
+        themeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(THEME_KEY, isChecked);
+            editor.apply();
+
+            AppCompatDelegate.setDefaultNightMode(isChecked ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
+        });
+    }
+
+    private void initLanguageSwitch(View rootView) {
+        languageSwitch = rootView.findViewById(R.id.switch1);
+        lsharedPreferences = requireActivity().getSharedPreferences(LANG_NAME, Context.MODE_PRIVATE);
+        String langPref = getLanguagePreference(); // Dil tercihini al
+        boolean isTurkish = langPref.equals("tr");
+        languageSwitch.setChecked(isTurkish);
+
+        languageSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            saveLanguagePreference(isChecked ? "tr" : "en"); // Dil tercihini kaydet
+            setAppLanguage(isChecked ? "tr" : "en"); // Uygulama dilini ayarla
+            restartActivity();
+        });
+    }
+
+    private void saveLanguagePreference(String languageCode) {
+        lsharedPreferences = requireActivity().getSharedPreferences(LANG_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = lsharedPreferences.edit();
         editor.putString(LANGUAGE_KEY, languageCode);
         editor.apply();
+    }
 
-        // Uygulamanın dilini dinamik olarak değiştirme işlemi
+    // Dil tercihini alıp işleme
+    private String getLanguagePreference() {
+        lsharedPreferences = requireActivity().getSharedPreferences(LANG_NAME, Context.MODE_PRIVATE);
+        // Varsayılan olarak en son kullanılan dil olan değeri döndür
+        return lsharedPreferences.getString(LANGUAGE_KEY, getLastUsedLanguage());
+    }
+
+    private String getLastUsedLanguage() {
+        // Burada en son kullanılan dilin bilgisini kaydettiğiniz bir şekilde alın
+        // Örneğin, başka bir SharedPreferences anahtarını kullanarak
+        lsharedPreferences = requireActivity().getSharedPreferences(LANG_NAME, Context.MODE_PRIVATE);
+        return lsharedPreferences.getString("LastUsedLanguage", "tr");
+    }
+
+    private void setAppLanguage(String languageCode) {
+        // Buradaki dil ayarı kodu ile uygulama dilini ayarlayın
         Locale locale = new Locale(languageCode);
         Locale.setDefault(locale);
 
         Configuration config = new Configuration();
         config.locale = locale;
-        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+        requireContext().getResources().updateConfiguration(config, requireContext().getResources().getDisplayMetrics());
+    }
 
+    private void restartActivity() {
+        requireActivity().recreate();
     }
 }
+
